d3b9b2f765806d02aa4135554879c934ba6f785b
rehmanbutt3656 <rehmanbutt3656@gmail.com>
2026-06-02 20:39:48 +0200

    Fix admin panel URL configuration

--- core/urls.py
+++ core/urls.py
@@ -5,16 +5,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-]
-from django.urls import path
-from . import views
-
-urlpatterns = [
+    path('admin/', admin.site.urls),  # This enables admin panel
     path('', views.home, name='home'),
     path('health/', views.health_check, name='health'),
 ]
